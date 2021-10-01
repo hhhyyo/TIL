9eb2ce78038d670dcb382ae1186c9dba40fc676b
hhhyyo <37561621+hhhyyo@users.noreply.github.com>
2021-10-01 16:49:21 +0900

    고차함수의 매개변수도 의미있게

--- javascript/Hof/3. 프로퍼티 정렬.js
+++ javascript/Hof/3. 프로퍼티 정렬.js
@@ -5,7 +5,7 @@ let todos = [
 ];
 
 const sortBy = (todos, key) =>
-  [...todos].sort((a, b) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0));
+  [...todos].sort((todo1, todo2) => todo1[key] > todo2[key] ? 1 : todo1[key] < todo2[key] ? -1 : 0);
 
 todos = sortBy(todos, 'id');
 console.log(todos);
