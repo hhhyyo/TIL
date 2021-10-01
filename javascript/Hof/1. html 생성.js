2c7e7b154c89e4a16f4d1f6c07410ca4ac500714
hhhyyo <37561621+hhhyyo@users.noreply.github.com>
2021-10-01 17:34:38 +0900

    Update 1. html 생성.js

--- javascript/Hof/1. html 생성.js
+++ javascript/Hof/1. html 생성.js
@@ -1,26 +1,26 @@
-// todos 배열을 인수로 전달받아 html 문자열을 생성해 반환하는 함수를 작성하라.
-
 const todos = [
   { id: 3, content: 'HTML', completed: false },
   { id: 2, content: 'CSS', completed: true },
   { id: 1, content: 'Javascript', completed: false }
 ];
 
-const render = todos => {
-  /* Do something */
-  let result = '';
-
-  todos.forEach(todo => {
-    const { id, content, completed } = todo;
-    result += `
-      <li id="${id}">
-        <label><input type="checkbox" ${
-          completed ? 'checked' : ''
-        }> ${content}</label>
-      </li>`;
-  });
-
-  return result;
-};
+const render = todos =>
+  todos.map(
+    ({ id, content, completed }) =>
+      `<li id="${id}">
+        <label><input type="checkbox" ${completed ? 'checked' : ''}>${content}</label>
+      </li>`
+  ).join('');
 
 console.log(render(todos));
+/*
+<li id="3">
+  <label><input type="checkbox">HTML</label>
+</li>
+<li id="2">
+  <label><input type="checkbox" checked>CSS</label>
+</li>
+<li id="1">
+  <label><input type="checkbox">Javascript</label>
+</li>
+*/
