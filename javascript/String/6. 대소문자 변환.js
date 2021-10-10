c9c7648e1a31b4797b094708afaf8b75f87f84d7
hhhyyo <gydnjsrh6923@korea.ac.kr>
2021-10-10 22:38:47 +0900

    fix : toggleCase

--- javascript/String/6. 대소문자 변환.js
+++ javascript/String/6. 대소문자 변환.js
@@ -1,6 +1,6 @@
 const toggleCase = str =>
-  str.replace(/[a-zA-Z]/g, match =>
-    /[a-z]/g.test(match) ? match.toUpperCase() : match.toLowerCase()
+  str.replace(/([a-z])|([A-Z])/g, (match, lowerCase) =>
+    lowerCase ? match.toUpperCase() : match.toLowerCase()
   );
 
 console.log(toggleCase('StuDY')); // => 'sTUdy'
